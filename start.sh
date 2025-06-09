50c1e7ffc0a9db0c869ebae2b431cfe271afc557
Mohd Irfan <165075053+mohdirfan-code@users.noreply.github.com>
2025-06-09 09:52:57 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,4 +5,6 @@ echo "🚀 Starting Zidio Services..."
 uvicorn api.main:app --host 0.0.0.0 --port 8000 &
 
 # Start Streamlit (on same network)
-streamlit run dashboard/app.py --server.port 8501 --server.address 0.0.0.0
+# streamlit run dashboard/app.py --server.port 8501 --server.address 0.0.0.0
+streamlit run dashboard/app.py --server.port=8000 --server.address=0.0.0.0
+
